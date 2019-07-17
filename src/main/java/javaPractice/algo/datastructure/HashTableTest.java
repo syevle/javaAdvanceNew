682042334798c0a6d7287e42ff3e507e8f562dd5
Santosh.Yevle <Santosh.Yevle@ril.com>
2019-07-17 11:42:29 +0530

    dfd

--- src/main/java/javaPractice/algo/datastructure/HashTableTest.java
+++ src/main/java/javaPractice/algo/datastructure/HashTableTest.java
@@ -19,133 +19,95 @@ import java.util.Scanner;
  The program output is also shown below.
  */
 /**
-
  * Java program to implement Hash Table
-
  **/
-
 
 
 import java.util.Scanner;
 
 
-
-class HashTable
-
-{
+class HashTable {
 
     int[] arr;
-
     int capacity;
 
 
+    /**
+     * constructor
+     **/
 
-    /** constructor **/
-
-    public HashTable(int capacity)
-
-    {
-
+    public HashTable(int capacity) {
         this.capacity = nextPrime(capacity);
-
         arr = new int[this.capacity];
 
     }
 
 
+    /**
+     * function to insert
+     **/
 
-    /** function to insert **/
-
-    public void insert(int ele)
-
-    {
-
+    public void insert(int ele) {
         arr[ele % capacity] = ele;
-
     }
 
 
+    /**
+     * function to clear
+     **/
 
-    /** function to clear **/
-
-    public void clear()
-
-    {
-
+    public void clear() {
         arr = new int[capacity];
-
     }
 
 
+    /**
+     * function contains
+     **/
 
-    /** function contains **/
-
-    public boolean contains(int ele)
-
-    {
-
+    public boolean contains(int ele) {
         return arr[ele % capacity] == ele;
-
     }
 
 
+    /**
+     * function to delete
+     **/
 
-    /** function to delete **/
-
-    public void delete(int ele)
-
-    {
-
+    public void delete(int ele) {
         if (arr[ele % capacity] == ele)
-
             arr[ele % capacity] = 0;
-
         else
-
             System.out.println("\nError : Element not found\n");
-
     }
 
 
+    /**
+     * Function to generate next prime number >= n
+     **/
 
-    /** Function to generate next prime number >= n **/
-
-    private static int nextPrime( int n )
-
-    {
-
+    private static int nextPrime(int n) {
         if (n % 2 == 0)
-
             n++;
-
-        for (; !isPrime(n); n += 2);
-
-
+        for (; !isPrime(n); n += 2) ;
 
         return n;
-
     }
 
 
+    /**
+     * Function to check if given number is prime
+     **/
 
-    /** Function to check if given number is prime **/
-
-    private static boolean isPrime(int n)
-
-    {
-
+    private static boolean isPrime(int n) {
         if (n == 2 || n == 3)
-
             return true;
 
         if (n == 1 || n % 2 == 0)
-
             return false;
 
         for (int i = 3; i * i <= n; i += 2)
-
             if (n % i == 0)
-
                 return false;
 
         return true;
@@ -153,18 +115,14 @@ class HashTable
     }
 
 
+    /**
+     * function to print hash table
+     **/
 
-    /** function to print hash table **/
-
-    public void printTable()
-
-    {
-
+    public void printTable() {
         System.out.print("\nHash Table = ");
-
         for (int i = 0; i < capacity; i++)
-
-            System.out.print(arr[i] +" ");
+            System.out.print(arr[i] + " ");
 
         System.out.println();
 
@@ -173,106 +131,58 @@ class HashTable
 }
 
 
+/**
+ * Class HashTableTest
+ **/
 
-/** Class HashTableTest **/
-
-public class HashTableTest
-
-{
-
-    public static void main(String[] args)
-
-    {
-
+public class HashTableTest {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Hash Table Test\n\n");
-
         System.out.println("Enter size");
-
         /** Make object of HashTable **/
-
-        HashTable ht = new HashTable(scan.nextInt() );
-
-
-
+        HashTable ht = new HashTable(scan.nextInt());
         char ch;
 
         /**  Perform HashTable operations  **/
 
-        do
-
-        {
-
+        do {
             System.out.println("\nHash Table Operations\n");
-
             System.out.println("1. insert ");
-
             System.out.println("2. remove");
-
             System.out.println("3. contains");
-
             System.out.println("4. clear");
 
 
-
             int choice = scan.nextInt();
-
-            switch (choice)
-
-            {
-
-                case 1 :
-
+            switch (choice) {
+                case 1:
                     System.out.println("Enter integer element to insert");
-
-                    ht.insert( scan.nextInt() );
-
+                    ht.insert(scan.nextInt());
                     break;
-
-                case 2 :
-
+                case 2:
                     System.out.println("Enter integer element to delete");
-
-                    ht.delete( scan.nextInt() );
-
+                    ht.delete(scan.nextInt());
                     break;
-
-                case 3 :
-
+                case 3:
                     System.out.println("Enter integer element to check if present");
-
-                    System.out.println("Contains : "+ ht.contains(scan.nextInt() ));
-
+                    System.out.println("Contains : " + ht.contains(scan.nextInt()));
                     break;
-
-                case 4 :
-
+                case 4:
                     ht.clear();
-
                     System.out.println("Hash Table Cleared\n");
-
                     break;
-
-                default :
-
+                default:
                     System.out.println("Wrong Entry \n ");
-
                     break;
 
             }
-
             /** Display hash table **/
-
             ht.printTable();
-
-
-
             System.out.println("\nDo you want to continue (Type y or n) \n");
-
             ch = scan.next().charAt(0);
 
-        } while (ch == 'Y'|| ch == 'y');
+        } while (ch == 'Y' || ch == 'y');
 
     }
 
