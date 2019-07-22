7a9fa5a98443ebfda58c9b52d0023723ae8fda52
Santosh.Yevle <Santosh.Yevle@ril.com>
2019-07-22 17:47:19 +0530

    dfdfd

---
+++ src/main/java/javaPractice/datastructure/Postorder/GFG.java
@@ -0,0 +1,70 @@
+package javaPractice.datastructure.Postorder;
+//Postorder traversal of Binary Tree without recursion and without stack
+// Java program or postorder traversal
+class GFG
+{
+
+    /* A binary tree node has data,
+        pointer to left child
+        and a pointer to right child */
+    static class Node
+    {
+        int data;
+        Node left, right;
+        boolean visited;
+    }
+
+    static void postorder( Node head)
+    {
+        Node temp = head;
+        while (temp != null &&
+                temp.visited == false)
+        {
+
+            // Visited left subtree
+            if (temp.left != null &&
+                    temp.left.visited == false)
+                temp = temp.left;
+
+                // Visited right subtree
+            else if (temp.right != null &&
+                    temp.right.visited == false)
+                temp = temp.right;
+
+                // Print node
+            else
+            {
+                System.out.printf("%d ", temp.data);
+                temp.visited = true;
+                temp = head;
+            }
+        }
+    }
+
+    static Node newNode(int data)
+    {
+        Node node = new Node();
+        node.data = data;
+        node.left = null;
+        node.right = null;
+        node.visited = false;
+        return (node);
+    }
+
+    /* Driver code*/
+    public static void main(String []args)
+    {
+        Node root = newNode(8);
+        root.left = newNode(3);
+        root.right = newNode(10);
+        root.left.left = newNode(1);
+        root.left.right = newNode(6);
+        root.left.right.left = newNode(4);
+        root.left.right.right = newNode(7);
+        root.right.right = newNode(14);
+        root.right.right.left = newNode(13);
+        postorder(root);
+    }
+}
+
+// This code is contributed by Arnab Kundu
