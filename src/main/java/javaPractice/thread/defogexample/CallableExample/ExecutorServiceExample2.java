80ab73d7670e965a59a84e00a2f12953e9877adc
Santosh.Yevle <Santosh.Yevle@ril.com>
2019-08-06 07:15:39 +0530

    dfdfd

--- src/main/java/javaPractice/thread/defogexample/CallableExample/ExecutorServiceExample2.java
+++ src/main/java/javaPractice/thread/defogexample/CallableExample/ExecutorServiceExample2.java
@@ -14,12 +14,8 @@ public class ExecutorServiceExample2 {
 
         public Double call() throws InterruptedException {
             System.out.println("Working on index " + index + "...");
-
             // pretend we're doing something long and complex here...
             Thread.sleep(2000);
-
-            System.out.println("Completed index " + index);
-
             // pretend this is the net result of the long, complex thing we did:
             return index * .001;
         }
@@ -27,24 +23,19 @@ public class ExecutorServiceExample2 {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        ExecutorService execs = Executors.newFixedThreadPool(5);
+        ExecutorService execs = Executors.newFixedThreadPool(1);
         Future<Double> result = execs.submit(new MyTask(10));
         // this is just a signal; it won't terminate until all tasks are completed
         execs.shutdown();
         System.out.println("Sent shutdown signal");
         execs.awaitTermination(60, TimeUnit.MINUTES);
 
-        System.out.println("Results: ");
-        System.out.println(result.get());
+        System.out.print("Results: "+result.get());
     }
 }
 
 /*
-Output
 Working on index 10...
 Sent shutdown signal
-Completed index 10
-Results:
-0.01
-
+Results: 0.01
  */
